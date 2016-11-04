05dae7a24607edcb87e9c131778e7cbb156d084a
Charlie <charliezon@gmail.com>
2016-11-04 18:10:23 +0800

    add a function to destroy pool to orm

--- www/orm.py
+++ www/orm.py
@@ -24,6 +24,13 @@ async def create_pool(loop, **kw):
         loop=loop
     )
 
+@asyncio.coroutine
+async def destory_pool(): #销毁连接池
+    global __pool
+    if __pool is not None:
+        __pool.close()
+        await  __pool.wait_closed()
+
 async def select(sql, args, size=None):
     log(sql, args)
     global __pool
