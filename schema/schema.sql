a4b1349dca0ebf89edf26ed6bf27d10598404c4f
Charlie <charliezon@gmail.com>
2016-11-04 15:06:40 +0800

    modify models and create schema

---
+++ schema/schema.sql
@@ -0,0 +1,62 @@
+-- schema.sql
+
+drop database if exists stock;
+
+create database stock;
+
+use stock;
+
+create table users (
+    `id` varchar(50) not null,
+    `email` varchar(50) not null,
+    `passwd` varchar(50) not null,
+    `admin` bool not null,
+    `name` varchar(50) not null,
+    `image` varchar(500) not null,
+    `created_at` real not null,
+    unique key `idx_email` (`email`),
+    key `idx_created_at` (`created_at`),
+    primary key (`id`)
+) engine=innodb default charset=utf8;
+
+create table accounts (
+    `id` varchar(50) not null,
+    `user_id` varchar(50) not null,
+    `name` varchar(50) not null,
+    `commission_rate` real not null,
+    `initial_funding` real not null,
+    `created_at` real not null,
+    key `idx_created_at` (`created_at`),
+    primary key (`id`)
+) engine=innodb default charset=utf8;
+
+create table account_records (
+    `id` varchar(50) not null,
+    `account_id` varchar(50) not null,
+    `market_condition` int(2) not null,
+    `stock_position` real not null,
+    `available_funding` real not null,
+    `total_stock_value` real not null,
+    `total_assets` real not null,
+    `float_profit_lost` real not null,
+    `total_profit` real not null,
+    `principle` real not null,
+    `created_at` real not null,
+    key `idx_created_at` (`created_at`),
+    primary key (`id`)
+) engine=innodb default charset=utf8;
+
+create table stock_hold_records (
+    `id` varchar(50) not null,
+    `account_record_id` varchar(50) not null,
+    `stock_code` varchar(50) not null,
+    `stock_name` varchar(50) not null,
+    `stock_amount` int(100) not null,
+    `stock_current_price` real not null,
+    `stock_buy_price` real not null,
+    `stock_sell_price` real not null,
+    `stock_buy_date` real not null,
+    `created_at` real not null,
+    key `idx_created_at` (`created_at`),
+    primary key (`id`)
+) engine=innodb default charset=utf8;
