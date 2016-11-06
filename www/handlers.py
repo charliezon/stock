9052fa2e75e549951acee9545992a98d1f56d69e
Charlie <charliezon@gmail.com>
2016-11-06 17:29:16 +0800

    created the first web page

--- www/handlers.py
+++ www/handlers.py
@@ -2,36 +2,20 @@
 
 # -*- coding: utf-8 -*-
 
-
-
 __author__ = 'Chaoliang Zhong'
-
-
 
 ' url handlers '
 
 import re, time, json, logging, hashlib, base64, asyncio
 
-
-
 from coroweb import get, post
 
-
-
-from models import User
-
-
+from models import User, next_id
 
 @get('/')
-
-async def index(request):
-
-    users = await User.findAll()
-
+def index(request):
+    user = User(id='1', name='Charlie', admin=True, email='b@example.com', passwd='1234567890', image='about:blank')
     return {
-
-        '__template__': 'test.html',
-
-        'users': users
-
+        '__template__': 'account.html',
+        'user': user
     }
