cc13726a90baddd6646364001d4c17e6249e7cd6
Charlie <charliezon@gmail.com>
2016-11-09 17:20:11 +0800

    implemented signin and register functions

--- schema/schema.sql
+++ schema/schema.sql
@@ -16,6 +16,7 @@ create table users (
     `name` varchar(50) not null,
     `image` varchar(500) not null,
     `created_at` real not null,
+    unique key `idx_name` (`name`),
     unique key `idx_email` (`email`),
     key `idx_created_at` (`created_at`),
     primary key (`id`)
--- www/app.py
+++ www/app.py
@@ -38,6 +38,8 @@ from coroweb import add_routes, add_static
 
 from config import configs
 
+from handlers import cookie2user, COOKIE_NAME
+
 
 def init_jinja2(app, **kw):
 
@@ -94,6 +96,25 @@ async def logger_factory(app, handler):
     return logger
 
 
+@asyncio.coroutine
+async def auth_factory(app, handler):
+    @asyncio.coroutine
+    async def auth(request):
+        logging.info('check user: %s %s' % (request.method, request.path))
+        request.__user__ = None
+        cookie_str = request.cookies.get(COOKIE_NAME)
+        if cookie_str:
+            user = await cookie2user(cookie_str)
+            if user:
+                logging.info('set current user: %s' % user.email)
+                request.__user__ = user
+# TODO
+#        if request.path.startswith('/manage/') and (request.__user__ is None or not request.__user__.admin):
+#            return web.HTTPFound('/signin')
+        return (await handler(request))
+    return auth
+
+
 
 async def data_factory(app, handler):
 
@@ -119,78 +140,45 @@ async def data_factory(app, handler):
 
 
 
+@asyncio.coroutine
 async def response_factory(app, handler):
-
-    async def response(request):
-
+    @asyncio.coroutine
+    def response(request):
         logging.info('Response handler...')
-
-        r = await handler(request)
-
+        r = yield from handler(request)
         if isinstance(r, web.StreamResponse):
-
             return r
-
         if isinstance(r, bytes):
-
             resp = web.Response(body=r)
-
             resp.content_type = 'application/octet-stream'
-
             return resp
-
         if isinstance(r, str):
-
             if r.startswith('redirect:'):
-
                 return web.HTTPFound(r[9:])
-
             resp = web.Response(body=r.encode('utf-8'))
-
             resp.content_type = 'text/html;charset=utf-8'
-
             return resp
-
         if isinstance(r, dict):
-
             template = r.get('__template__')
-
             if template is None:
-
                 resp = web.Response(body=json.dumps(r, ensure_ascii=False, default=lambda o: o.__dict__).encode('utf-8'))
-
                 resp.content_type = 'application/json;charset=utf-8'
-
                 return resp
-
             else:
-
+                r['__user__'] = request.__user__
                 resp = web.Response(body=app['__templating__'].get_template(template).render(**r).encode('utf-8'))
-
                 resp.content_type = 'text/html;charset=utf-8'
-
                 return resp
-
-        if isinstance(r, int) and r >= 100 and r < 600:
-
-            return web.Response(r)
-
+        if isinstance(r, int) and t >= 100 and t < 600:
+            return web.Response(t)
         if isinstance(r, tuple) and len(r) == 2:
-
             t, m = r
-
             if isinstance(t, int) and t >= 100 and t < 600:
-
                 return web.Response(t, str(m))
-
         # default:
-
         resp = web.Response(body=str(r).encode('utf-8'))
-
         resp.content_type = 'text/plain;charset=utf-8'
-
         return resp
-
     return response
 
 
@@ -226,7 +214,7 @@ async def init(loop):
 
     app = web.Application(loop=loop, middlewares=[
 
-        logger_factory, response_factory
+        logger_factory, auth_factory, response_factory
 
     ])
 
