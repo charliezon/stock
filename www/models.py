e60edc751ece211348980664cc81e616641607c0
Charlie <charliezon@gmail.com>
2016-11-29 15:26:18 +0800

    handle negative for round_float

--- www/models.py
+++ www/models.py
@@ -23,14 +23,22 @@ def today():
 def convert_date(date):
     return datetime.datetime.strptime(date,'%Y-%m-%d')
 
-def round_float(f, pos=2):
+def round_float(g, pos=2):
+    if g<0:
+        f = -g
+    else:
+        f = g
     p1 = pow(D('10'), D(str(pos+1)))
     last = D(str(int(D(str(f))*p1)))%D('10')
     p = pow(D('10'), D(str(pos)))
     if last >= 5:
-        return float(math.ceil(D(str(f))*p)/p)
+        result = float(math.ceil(D(str(f))*p)/p)
     else:
-        return float(math.floor(D(str(f))*p)/p)
+        result = float(math.floor(D(str(f))*p)/p)
+    if g<0:
+        return -result
+    else:
+        return result
 
 
 class User(Model):
