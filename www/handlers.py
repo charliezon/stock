85c85b804b72c6a038eb46131476fdef10c34053
charliezon <charliezons@qq.com>
2017-03-26 13:37:01 +0800

    implement remove account and batch remove functions

--- www/handlers.py
+++ www/handlers.py
@@ -870,6 +870,34 @@ async def api_advanced_create_account(request, *, name, commission_rate, initial
     return account
 
 @asyncio.coroutine
+@post('/api/remove/accounts')
+async def api_remove_account(request, *, account_id):
+    must_log_in(request)
+    if not account_id or not account_id.strip():
+        return '删除账户失败(1)'
+    account = await Account.find(account_id)
+    if not account:
+        return '删除账户失败(2)'
+    if (not request.__user__.admin and request.__user__.id != account.user_id):
+        return '删除账户失败(3)'
+
+    async with get_pool().get() as conn:
+        await conn.begin()
+        try:
+            await AccountAssetChange.removeAll(conn, 'account_id=?', [account.id])
+            await StockTradeRecord.removeAll(conn, 'account_id=?', [account.id])
+            account_records = await AccountRecord.findAll('account_id=?', [account.id])
+            for account_record in account_records:
+                await StockHoldRecord.removeAll(conn, 'account_record_id=?', [account_record.id])
+            await AccountRecord.removeAll(conn, 'account_id=?', [account.id])
+            await account.remove(conn)
+            await conn.commit()
+        except BaseException as e:
+            await conn.rollback()
+            return '删除账户失败(4)'
+    return '删除账户成功'
+
+@asyncio.coroutine
 @post('/api/buy')
 async def api_buy(request, *, stock_name, stock_code, stock_price, stock_amount, date, account_id):
     must_log_in(request)
